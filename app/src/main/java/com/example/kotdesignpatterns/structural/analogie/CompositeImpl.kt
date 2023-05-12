84d6d840143fec10bfc182ba85e59fec3305778b
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-12 15:30:31 +0200

    CompositeImpl added

---
+++ app/src/main/java/com/example/kotdesignpatterns/structural/analogie/CompositeImpl.kt
@@ -0,0 +1,65 @@
+package com.example.kotdesignpatterns.structural.analogie
+
+// Interface représentant une composante (cellule, tissu ou organe)
+interface Composante {
+    fun afficherStructure()
+}
+
+// Classe représentant une cellule
+class Cellule(private val nom: String) : Composante {
+    override fun afficherStructure() {
+        println("Cellule : $nom")
+    }
+}
+
+// Classe représentant un tissu composé de plusieurs cellules
+class Tissu(private val nom: String) : Composante {
+    private val cellules: MutableList<Composante> = mutableListOf()
+
+    fun ajouterCellule(cellule: Composante) {
+        cellules.add(cellule)
+    }
+
+    override fun afficherStructure() {
+        println("Tissu : $nom")
+        for (cellule in cellules) {
+            cellule.afficherStructure()
+        }
+    }
+}
+
+// Classe représentant un organe composé de plusieurs tissus et éventuellement d'autres organes
+class Organe(private val nom: String) : Composante {
+    private val composantes: MutableList<Composante> = mutableListOf()
+
+    fun ajouterComposante(composante: Composante) {
+        composantes.add(composante)
+    }
+
+    override fun afficherStructure() {
+        println("Organe : $nom")
+        for (composante in composantes) {
+            composante.afficherStructure()
+        }
+    }
+}
+
+// Fonction de test
+fun main() {
+    val cellule1 = Cellule("Cellule 1")
+    val cellule2 = Cellule("Cellule 2")
+    val cellule3 = Cellule("Cellule 3")
+
+    val tissuA = Tissu("Tissu A")
+    tissuA.ajouterCellule(cellule1)
+    tissuA.ajouterCellule(cellule2)
+
+    val tissuB = Tissu("Tissu B")
+    tissuB.ajouterCellule(cellule3)
+
+    val organe = Organe("Organe")
+    organe.ajouterComposante(tissuA)
+    organe.ajouterComposante(tissuB)
+
+    organe.afficherStructure()
+}
